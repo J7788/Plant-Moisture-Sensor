67213c444682fc96a8793eb6a4125257f6401d06
J7788 <j.h.lunz@gmail.com>
2022-08-13 16:47:03 -0500

    Added in functionality for client and server

--- RemotePlantMoistureSensor/RemotePlantMoistureSensor.ino
+++ RemotePlantMoistureSensor/RemotePlantMoistureSensor.ino
@@ -1,25 +1,49 @@
 #include <ESP8266WiFi.h>
 
+char network[] = "MOTO7466";
+char pass[] = "zpzdf2evu9";
+byte ip[4] = {192, 168, 0, 12};
+char buffer[256];
+WiFiClient client;
 
 void setup() {
-  // put your setup code here, to run once:
+  //Setting up the wifi connection
   Serial.begin(9600);
   Serial.println("\nRemote Plant Moisture sensor v0");
 
-  WiFi.begin("MOTO7466", "zpzdf2evu9");
+  WiFi.begin(network, pass);
 
   Serial.print("Connecting");
 
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println();
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  Serial.println("Attemping Connection to main computer now...");
+  if (client.connect(ip, 1492)) {
+    Serial.println("Connected!");
+    client.println("This is a test coming from an esp8266. Your moms a hoe");
+    Serial.println("Message sent. Waiting on response...");
+    byte next_byte;
+    while(next_byte = client.read()){
+      Serial.print(next_byte);
+    }
+    Serial.println();
+    buffer[256] = '\0';
+    Serial.println(buffer);
+    client.stop();
+    Serial.print("Connection closed");
+  }
+  else {
+    Serial.println("Connection failed");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
 }
--- UDPServer.cpp
+++ UDPServer.cpp
@@ -1,4 +1,55 @@
 #include <iostream>
-void main(){
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+using namespace std;
+int main(){
+    struct sockaddr_in addy;
+    //Creating the socket and address
+    addy.sin_family = AF_INET;
+    addy.sin_addr.s_addr = htonl(INADDR_ANY);
+    addy.sin_port = htons(1492);
 
+    int socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if(socket_fd < 0){
+        perror("Error while creating socket");
+        exit(EXIT_FAILURE);
+    }
+    //Binding the socket to port number 1492
+    if(bind(socket_fd, (struct sockaddr*)&addy, sizeof(addy)) > 0){
+        perror("Error while binding socket");
+        exit(EXIT_FAILURE);
+    }
+
+
+    //Turn on listening for the server
+    if(listen(socket_fd, 5) < 0){
+        perror("Error while listening");
+        exit(EXIT_FAILURE);
+    }
+
+    //Accepting a new connection
+    struct sockaddr_in client;
+    unsigned int client_sock_len;
+
+    int new_sock = accept(socket_fd, (struct sockaddr*)&client, &client_sock_len);
+    char buffer[256];
+
+    //Reading the data from the connection
+    if(read(new_sock, buffer, 255) < 0){
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+    buffer[256] = '\0';
+    printf(buffer);
+
+    //Writing a return message to the connection
+    char return_message[256] = "This is a return message to the esp8266"; 
+    if(write(new_sock, return_message, 256) < 0){
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
 }
